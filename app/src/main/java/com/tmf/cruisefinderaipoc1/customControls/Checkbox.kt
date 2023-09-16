27b60e36400709697cfc727b0e4c9096eb31a2e7
Mike Mayfield <mike@themayfields.com>
2023-09-15 20:24:48 -0700

    Added Date control.

--- app/src/main/java/com/tmf/cruisefinderaipoc1/customControls/Checkbox.kt
+++ app/src/main/java/com/tmf/cruisefinderaipoc1/customControls/Checkbox.kt
@@ -19,24 +19,29 @@ fun Checkbox(
     control: Control,
     recomposeTrigger: Int,
     modifier: Modifier = Modifier,
+    useCard: Boolean = true,
     onValueChange: (changedControl: Control) -> Unit = {}
 ) {
     if (recomposeTrigger == -1) return  //NOTE: This will never be true. Used to force a "use" of recomposeTrigger so that Compose will call this method when trigger is changed
 
     Log.v("Checkbox", "Compose ${control.controlIdLc}")
 
-    Row(verticalAlignment = Alignment.CenterVertically) {
-        Checkbox(checked = (control.liveValue == "checked"),
-            modifier = modifier.padding(start = 2.dp),
-            onCheckedChange = {
-                control.liveValue = if (it) "checked" else "unchecked"
-                onValueChange(control)
-            }
-        )
+    if (useCard) {
 
-        Text(
-            modifier = Modifier.padding(start = 2.dp),
-            text = control.Label
-        )
+    } else {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Checkbox(checked = (control.liveValue == "checked"),
+                modifier = modifier.padding(start = 2.dp),
+                onCheckedChange = {
+                    control.liveValue = if (it) "checked" else "unchecked"
+                    onValueChange(control)
+                }
+            )
+
+            Text(
+                modifier = Modifier.padding(start = 2.dp),
+                text = control.Label
+            )
+        }
     }
 }
